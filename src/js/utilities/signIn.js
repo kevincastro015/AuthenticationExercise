cc41017806686b4233c6c98630c1712b7f242546
kevincastro015 <kevin.castro015@mymdc.net>
2021-07-31 01:12:50 +0000

    appContextFluxSignIn

--- src/js/utilities/signIn.js
+++ src/js/utilities/signIn.js
@@ -1,1 +1,6 @@
 // Declare a function to signIn
+import firebase from "firebase/app";
+
+export const signIn = (email, password) => {
+	return firebase.auth().signInWithEmailAndPassword(email, password);
+};
